6e0d07a04795833d1917b3698a00d34fc70142cf
vvmalyuga <1132236050@pfur.ru>
2023-10-16 15:26:12 +0300

    feat(main): add files lab-4

---
+++ labs/lab04/lab4.asm
@@ -0,0 +1,19 @@
+; lab4.asm
+SECTION .data
+	name: DB 'Малюга Валерия',10
+	
+	nameLen: EQU $-name
+	
+SECTION .text 
+	GLOBAL _start
+	
+_start:
+	mov eax,4
+	mov ebx,1
+	mov ecx,name
+	mov edx,nameLen
+	int 80h
+	
+	mov eax,1
+	mov ebx,0
+	int 80h
